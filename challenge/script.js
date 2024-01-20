8cea8cac3b595427941178d951f261a8fa03715c
UMSC_Official <cao_junkai@qq.com>
2024-01-21 01:54:54 +0800

    Update

--- challenge/script.js
+++ challenge/script.js
@@ -1,11 +1,5 @@
 totalQueNum = 10, userSelect = '', correctQueNum = 0, currentQueNum = 0;
 queList = [], userAns = [], correctAns = [], result = [], ansCard = [], queSource = [];
-document.title = totalQueNum + ' Questions Challenge';
-document.getElementById("optnA").addEventListener("click", function () { sel(0) });
-document.getElementById("optnB").addEventListener("click", function () { sel(1) });
-document.getElementById("optnC").addEventListener("click", function () { sel(2) });
-document.getElementById("optnD").addEventListener("click", function () { sel(3) });
-document.getElementById("optnE").addEventListener("click", function () { sel(4) });
 function ifsame(m, n) {
 	if (queList[m][0] == queList[n][0] && queList[m][1] == queList[n][1]) {
 		return true;
@@ -18,13 +12,16 @@ function genQue(num) {
 	for (var j = 0; j < i; j++) {
 		if (ifsame(num, j) == true) {
 			genQueMain(num);
-		}
-	}
+		};
+	};
 };
 function genQueMain(num) {
 	queList[num][0] = Math.floor(Math.random() * 13);
 	queList[num][1] = Math.floor(Math.random() * 5);
 	queList[num][2] = data[queList[num][0]].pbls[queList[num][1]].asw;
+};
+function ltr(num) {
+	return String.fromCharCode(num + 65);
 };
 function sel(opt) {
 	function clsAdd(idO, idP, idQ, idR) {
@@ -38,55 +35,44 @@ function sel(opt) {
 		document.getElementById(idR).classList.add("unseled");
 	};
 	if (opt == 0) {
-		clsAdd("optnB", "optnC", "optnD", "optnE");
-		document.getElementById("optnA").classList.add("seled");
-		document.getElementById("optnA").classList.remove("unseled");
-		userSelect = 'A';
+		clsAdd("optn1", "optn2", "optn3", "optn4");
+		document.getElementById("optn0").classList.add("seled");
+		document.getElementById("optn0").classList.remove("unseled");
 	} else if (opt == 1) {
-		clsAdd("optnA", "optnC", "optnD", "optnE");
-		document.getElementById("optnB").classList.add("seled");
-		document.getElementById("optnB").classList.remove("unseled");
-		userSelect = 'B';
+		clsAdd("optn0", "optn2", "optn3", "optn4");
+		document.getElementById("optn1").classList.add("seled");
+		document.getElementById("optn1").classList.remove("unseled");
 	} else if (opt == 2) {
-		clsAdd("optnA", "optnB", "optnD", "optnE");
-		document.getElementById("optnC").classList.add("seled");
-		document.getElementById("optnC").classList.remove("unseled");
-		userSelect = 'C';
+		clsAdd("optn0", "optn1", "optn3", "optn4");
+		document.getElementById("optn2").classList.add("seled");
+		document.getElementById("optn2").classList.remove("unseled");
 	} else if (opt == 3) {
-		clsAdd("optnA", "optnB", "optnC", "optnE");
-		document.getElementById("optnD").classList.add("seled");
-		document.getElementById("optnD").classList.remove("unseled");
-		userSelect = 'D';
+		clsAdd("optn0", "optn1", "optn2", "optn4");
+		document.getElementById("optn3").classList.add("seled");
+		document.getElementById("optn3").classList.remove("unseled");
 	} else if (opt == 4) {
-		clsAdd("optnA", "optnB", "optnC", "optnD");
-		document.getElementById("optnE").classList.add("seled");
-		document.getElementById("optnE").classList.remove("unseled");
-		userSelect = 'E';
+		clsAdd("optn0", "optn1", "optn2", "optn3");
+		document.getElementById("optn4").classList.add("seled");
+		document.getElementById("optn4").classList.remove("unseled");
 	};
+	userSelect = ltr(opt);
 	document.getElementById("continue").disabled = false;
 };
 function setQue() {
 	document.getElementById("question").innerHTML = '<span class="emsp">&emsp;</span>'
-		+ data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[0]
-		+ '<br /><span class="emsp">&emsp;</span>'
+		+ data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[0] + '<br /><span class="emsp">&emsp;</span>'
 		+ data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[1];
-	document.getElementById("textA").innerHTML = data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[2];
-	document.getElementById("textB").innerHTML = data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[3];
-	document.getElementById("textC").innerHTML = data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[4];
-	document.getElementById("textD").innerHTML = data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[5];
+	document.getElementById("text0").innerHTML = data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[2];
+	document.getElementById("text1").innerHTML = data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[3];
+	document.getElementById("text2").innerHTML = data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[4];
+	document.getElementById("text3").innerHTML = data[queList[currentQueNum][0]].pbls[queList[currentQueNum][1]].eng[5];
 	document.getElementById("continue").disabled = true;
 	document.getElementById("prog-bar").style.width = (currentQueNum * 10 / totalQueNum * 10).toFixed() + '%';
 	document.getElementById("prog-bar").innerHTML = (currentQueNum * 10 / totalQueNum * 10).toFixed() + '%';
-	if (userAns[currentQueNum] == 'A') {
-		sel(0);
-	} else if (userAns[currentQueNum] == 'B') {
-		sel(1);
-	} else if (userAns[currentQueNum] == 'C') {
-		sel(2);
-	} else if (userAns[currentQueNum] == 'D') {
-		sel(3);
-	} else if (userAns[currentQueNum] == 'E') {
-		sel(4);
+	for (var i = 0; i < 5; i++) {
+		if (userAns[currentQueNum] == ltr(i)) {
+			sel(i);
+		};
 	};
 	if (currentQueNum == 0) {
 		document.getElementById("prog-bar").innerHTML = '';
@@ -97,16 +83,16 @@ function setQue() {
 	// console.log(userAns);
 };
 function clsRst() {
-	document.getElementById("optnA").classList.remove("seled");
-	document.getElementById("optnB").classList.remove("seled");
-	document.getElementById("optnC").classList.remove("seled");
-	document.getElementById("optnD").classList.remove("seled");
-	document.getElementById("optnE").classList.remove("seled");
-	document.getElementById("optnA").classList.add("unseled");
-	document.getElementById("optnB").classList.add("unseled");
-	document.getElementById("optnC").classList.add("unseled");
-	document.getElementById("optnD").classList.add("unseled");
-	document.getElementById("optnE").classList.add("unseled");
+	document.getElementById("optn0").classList.remove("seled");
+	document.getElementById("optn1").classList.remove("seled");
+	document.getElementById("optn2").classList.remove("seled");
+	document.getElementById("optn3").classList.remove("seled");
+	document.getElementById("optn4").classList.remove("seled");
+	document.getElementById("optn0").classList.add("unseled");
+	document.getElementById("optn1").classList.add("unseled");
+	document.getElementById("optn2").classList.add("unseled");
+	document.getElementById("optn3").classList.add("unseled");
+	document.getElementById("optn4").classList.add("unseled");
 };
 function forward() {
 	currentQueNum++;
@@ -117,7 +103,7 @@ function forward() {
 		info();
 	} else {
 		setQue();
-	}
+	};
 };
 function backward() {
 	currentQueNum--;
@@ -126,21 +112,12 @@ function backward() {
 };
 function debug() {
 	for (var i = 0; i < totalQueNum; i++) {
-		var rdm = Math.floor(Math.random() * 4);
-		if (rdm == 0) {
-			userAns[i] = 'A';
-		} else if (rdm == 1) {
-			userAns[i] = 'B';
-		} else if (rdm == 2) {
-			userAns[i] = 'C';
-		} else if (rdm == 3) {
-			userAns[i] = 'D';
-		};
+		userAns[i] = ltr(Math.floor(Math.random() * 4));
 	};
 	info();
 };
 function info() {
-	alert('作答完毕，点击确定或关闭查看结果');
+	alert('作答完毕，点击确定或关闭查看结果'); //debug的时候把这里注释掉
 	if (document.getElementById("header")) {
 		document.getElementById("header").remove();
 		document.getElementById("question").remove();
@@ -187,22 +164,23 @@ function info() {
 	for (var i = 0; i < totalQueNum; i++) {
 		writeTable += '<tr><td>'
 			+ (i + 1) + '</td><td>'
-			+ ansCard[0][i]
-			+ '</td><td>'
-			+ ansCard[1][i]
-			+ '</td><td>'
-			+ data[ansCard[3][i][0]].paperTitle
-			+ '</td><td>'
-			+ (ansCard[3][i][1] + 1)
-			+ '</td></tr>';
+			+ ansCard[0][i] + '</td><td>'
+			+ ansCard[1][i] + '</td><td>'
+			+ data[ansCard[3][i][0]].paperTitle + '</td><td>'
+			+ (ansCard[3][i][1] + 1) + '</td></tr>';
 	};
 	document.getElementById("tbody").innerHTML = writeTable;
+};
+for (let i = 0; i < 5; i++) {
+	document.getElementById("optn" + i).addEventListener("click", function () { sel(i) });
 };
 for (var i = 0; i < totalQueNum; i++) {
 	userAns.push('');
 	queList.push([]);
 	genQue(i);
 };
+document.title = totalQueNum + ' Questions Challenge';
 console.log(queList);
 clsRst();
 setQue();
+// debug(); //debug的时候把这里取消注释
